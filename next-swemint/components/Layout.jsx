3d4a69203e4e0719407c50df25fcec7f194adbd2
nandantyagi <nandantyagi@gmail.com>
2022-07-27 22:51:52 +0200

    Adjust dasboard styles

--- next-swemint/components/Layout.jsx
+++ next-swemint/components/Layout.jsx
@@ -4,32 +4,28 @@ import ConnectionMsg from './ConnectionMsg';
 import Footer from './Footer';
 import Main from './Main';
 import styles from '../styles/Layout.module.css';
+import bgImageDesktop from '../public/img/swemint-bg-1.jpg';
 import bgImageMobile from '../public/img/bg-1-mobile.png';
-import bgImageDesktop from '../public/img/swemint-db-bg-1.jpg';
 
 
 const Layout = () => {
-    const [windowSize, setWindowSize] = useState('');
+    const [windowSize, setWindowSize] = useState();
     const [isMdViewport, setIsMdViewport] = useState(null);
 
     function handelResize() {
-        setWindowSize(window.innerWidth)
+        if (window.innerWidth >= 682) {
+            setIsMdViewport(true)
+        }
+        if (window.innerWidth < 682) {
+            setIsMdViewport(false)
+        }
     }
 
     useEffect(() => {
+        handelResize();
         window.addEventListener('resize', () => handelResize())
         return window.removeEventListener('resize', handelResize)
-    }, [windowSize])
-
-    useEffect(() => {
-        if (windowSize >= 682) {
-            setIsMdViewport(true)
-        }
-        if (windowSize < 682) {
-            setIsMdViewport(prev => false)
-        }
-    }, [windowSize])
-
+    }, [])
 
     return (
         <>
