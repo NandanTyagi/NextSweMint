4e24ddda5beef1f43426b7a7c9ff4157414370f4
nandantyagi <nandantyagi@gmail.com>
2022-07-31 02:30:34 +0200

    Fix next-head save file

--- next-swemint/components/NextHead.jsx
+++ next-swemint/components/NextHead.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export const NextHead = ({title}) => {
+export const NextHead = ({ title }) => {
     return (
         <Head>
             <title>Swemint.io | {title}</title>
