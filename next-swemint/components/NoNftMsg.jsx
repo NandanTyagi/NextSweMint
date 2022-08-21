7b357cddd6dbdc6ee82711a9ef58a1fa5fb18850
nandantyagi <nandantyagi@gmail.com>
2022-08-21 04:39:44 +0200

    Fix styles non msg2

--- next-swemint/components/NoNftMsg.jsx
+++ next-swemint/components/NoNftMsg.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from '../styles/NoNftMsg.module.css'
+import styles from '../styles/NonNftMsg.module.css'
 const NoNftMsg = () => {
     return (
         <>
