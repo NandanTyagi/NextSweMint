cb2ddfafa0a9d864041775b89b5674bf7791b738
nandantyagi <nandantyagi@gmail.com>
2022-08-21 04:37:11 +0200

    Fix styles non msg

--- next-swemint/components/NoNftMsg.jsx
+++ next-swemint/components/NoNftMsg.jsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
+import styles from '../styles/NoNftMsg'
 const NoNftMsg = () => {
     return (
         <>
         <h3></h3>
-        <div>
+        <div className={styles.center}>
         <h3 style={{textAlign:'center', color:"white"}}>No NFTS to display</h3>
-        <p style={{color:"white"}}>You need to connect wallet to se your NFTS<span><Link href="/"> [Go connect]</Link></span></p>
-        
+        <p style={{color:"white"}}>You need to connect wallet to se your NFTS</p>
+        <Link href="/"> [Go connect]</Link>
         </div>
         <h3></h3>
         </>
