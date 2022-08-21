a8a82bf3ca5d7f793ea71b0da05e5f424ae33861
nandantyagi <nandantyagi@gmail.com>
2022-08-21 03:03:01 +0200

    Add link

--- next-swemint/components/NoNftMsg.jsx
+++ next-swemint/components/NoNftMsg.jsx
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 const NoNftMsg = () => {
     return (
         <>
         <h3></h3>
         <div>
         <h3 style={{textAlign:'center'}}>No NFTS to display</h3>
-        <p>You need to connect wallet to se your NFTS<span><a href="/"> [Go connect]</a></span></p>
+        <p>You need to connect wallet to se your NFTS<span><Link href="/"> [Go connect]</Link></span></p>
         
         </div>
         <h3></h3>
